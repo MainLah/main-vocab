82b5bf1f12054bd2237c25406bc0647e57590a20
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2025-06-17 12:30:47 +0700

    feat: auth using NextAuth

---
+++ src/components/AuthButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+
+type AuthButtonProps = {
+  className?: string;
+};
+
+export function AuthButton({ className = "" }: AuthButtonProps) {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <Button onClick={() => signOut()} className={className}>
+        Sign out
+      </Button>
+    );
+  }
+  return (
+    <Button onClick={() => signIn("discord")} className={className}>
+      Sign in with Discord
+    </Button>
+  );
+}
